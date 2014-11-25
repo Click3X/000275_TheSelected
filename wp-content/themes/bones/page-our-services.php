ce01036f7eef775dc2d5b3eb00c0ac8dac841b9c
Wen Zhang <wen@click3x.com>
2014-11-25 12:16:07 -0500

    add heart template

---
+++ wp-content/themes/bones/page-our-services.php
@@ -0,0 +1,51 @@
+<?php
+/*
+ Template Name: Our Services
+*/
+?>
+
+<?php get_header(); ?>
+
+			<div id="content">
+
+				<div id="inner-content" class="wrap cf">
+
+					<div id="main" class="m-all cf" role="main">
+
+						<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+						<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
+
+
+							<section class="entry-content cf" itemprop="articleBody">
+								<?php the_content(); ?>
+								<img class="circle" src="<?php echo get_template_directory_uri(); ?>/library/images/circle.png">
+								<div class="heart-container">
+									<div class="cubeWrapper">
+									    <div class="rotator">
+									        <div class="cube">
+									          <img src="<?php echo get_template_directory_uri(); ?>/library/images/heart.png">
+									          
+									        </div>
+									    </div>
+									</div>
+								</div>
+								
+							</section>
+
+
+						</article>
+
+						<?php endwhile; else : ?>
+
+						<?php endif; ?>
+
+					</div>
+
+
+				</div>
+
+			</div>
+
+
+<?php get_footer(); ?>
