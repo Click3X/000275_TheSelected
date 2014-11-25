03a445ed4a315302dac636dade7a3e308f232b69
Wen Zhang <wen@click3x.com>
2014-11-25 17:47:43 -0500

    labels added on our services page

--- wp-content/themes/bones/page-our-services.php
+++ wp-content/themes/bones/page-our-services.php
@@ -4,7 +4,13 @@
 */
 ?>
 
-<?php get_header(); ?>
+<?php get_header(); 
+	
+$label_str = get_field('label-strategy');
+$label_com = get_field('label-commerce');
+$label_cre = get_field('label-creativity');
+
+?>
 
 			<div id="content" style="overflow-x: hidden">
 
@@ -18,11 +24,20 @@
 
 
 							<section class="entry-content cf" itemprop="articleBody">
-								<?php the_content(); ?>
+								<?php the_content(); 
+
+
+								?>
 
 								<div class="short-divider"></div>
+
+								<!-- WHOLE WRAPPER -->
 								<div class="circle-wrapper">
+
+									<!-- CIRCLE WRAPPER -->
 									<img class="circle" src="<?php echo get_template_directory_uri(); ?>/library/images/circle.png">
+
+									<!-- HEART WRAPPER -->
 									<div class="heart-container">
 									<div class="cubeWrapper">
 										<div class="rotator">
@@ -33,6 +48,12 @@
 										    </div>
 										</div>
 									</div>
+
+									<!-- TEXT LAYER -->
+									<img class="lines" src="<?php echo get_template_directory_uri(); ?>/library/images/lines.png">
+									<div class="circle-labels label-str"><div></div> = <?php echo $label_str; ?></div>
+									<div class="circle-labels label-com"><div></div> = <?php echo $label_com; ?></div>
+									<div class="circle-labels label-cre"><div style="background:#daa250"></div> = <?php echo $label_cre; ?></div>
 								</div>
 
 								
