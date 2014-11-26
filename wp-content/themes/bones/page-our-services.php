a2d4294e40ae6a2535aab2dcf13383d9b170cab4
Wen Zhang <wen@click3x.com>
2014-11-26 16:23:52 -0500

    our services more css tweaks - responsive

--- wp-content/themes/bones/page-our-services.php
+++ wp-content/themes/bones/page-our-services.php
@@ -21,6 +21,14 @@ $black5 = get_field('product_innovation');
 $black6 = get_field('trade_engagement');
 $black7 = get_field('sales_channel');
 
+// GOLDEN
+$gold1 = get_field('key_influencer');
+$gold2 = get_field('experimental_premium');
+$gold3 = get_field('digital_creative');
+$gold4 = get_field('advertising_creative');
+$gold5 = get_field('partnerships_media');
+$gold6 = get_field('360_brand');
+
 
 ?>
 
@@ -61,6 +69,7 @@ $black7 = get_field('sales_channel');
 										</div>
 									</div>
 
+
 									<!-- TEXT LAYER -->
 									<div class="heart-text"><?php echo $heart_text; ?></div>
 									<img class="lines" src="<?php echo get_template_directory_uri(); ?>/library/images/lines.png">
@@ -68,14 +77,23 @@ $black7 = get_field('sales_channel');
 									<div class="circle-labels label-com"><div style="background:#5d5e61"></div> = <?php echo $label_com; ?></div>
 									<div class="circle-labels label-cre"><div style="background:#daa250"></div> = <?php echo $label_cre; ?></div>
 
-									<!-- BLACK & GOLDEN TEXT -->
+									<!-- BLACK & GREY & GOLDEN TEXT -->
 									<div class="float-text black1"><?php echo $black1; ?></div>
 									<div class="float-text black2"><?php echo $black2; ?></div>
 									<div class="float-text black3"><?php echo $black3; ?></div>
 									<div class="float-text black4"><?php echo $black4; ?></div>
+									<!-- grey -->
 									<div class="float-text black5"><?php echo $black5; ?></div>
 									<div class="float-text black6"><?php echo $black6; ?></div>
 									<div class="float-text black7"><?php echo $black7; ?></div>
+									<!-- golden -->
+									<div class="float-text gold1"><?php echo $gold1; ?></div>
+									<div class="float-text gold2"><?php echo $gold2; ?></div>
+									<div class="float-text gold3"><?php echo $gold3; ?></div>
+									<div class="float-text gold4"><?php echo $gold4; ?></div>
+									<div class="float-text gold5"><?php echo $gold5; ?></div>
+									<div class="float-text gold6"><?php echo $gold6; ?></div>
+
 								</div>
 
 								<p class="bottom-text"><?php echo $bottom_text; ?></p>
