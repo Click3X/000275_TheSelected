8776b30494b37384a07e72693f509266eb8578d3
Charles Howard Davis III <charles@click3x.com>
2014-12-02 15:57:29 -0500

    Added in new marquee static html to replace WSIWYG marquee page output

---
+++ wp-content/themes/bones/php/marquee/marquee.php
@@ -0,0 +1,23 @@
+<?php 
+// MARQUEE PHP 
+// STATIC CODE FOR MAIN MARQUEE - TO REPLACE THE MARQUEE 'PAGE IN ADMIN
+?>
+
+<div class="marquee cf shadow">
+    <div class="marquee-holder cf">
+        
+        <div class="logo-holder">
+            <div class="logo marquee-logo"></div>
+        </div>
+
+        <h2 class="marq-2"><span style="font-family: minion-pro,serif;">the</span></h2>
+
+        <h1>SELECTED</h1>
+
+        <hr>
+
+        <h2 class="gold-headline">DESIGNERS<span style="font-family: minion-pro,serif;">of</span>GROWTH</h2>
+        <h2 class="white-headline">STAND OUT &amp; GET SELECTED<span class="bullet">·</span>ENGAGE &amp; PROSPER</h2>
+
+    </div> <!-- END MARQUEE-HOLDER -->
+</div><!-- END MARQUEE -->
