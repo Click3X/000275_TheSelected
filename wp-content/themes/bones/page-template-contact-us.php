d36da0529c4026af100fe893ca27b6ae5ba01f68
Charles Howard Davis III <charles@click3x.com>
2014-12-03 16:36:14 -0500

    Contact Page in the works

--- wp-content/themes/bones/page-template-contact-us.php
+++ wp-content/themes/bones/page-template-contact-us.php
@@ -12,14 +12,28 @@ Template Name: Contact Us
 
 		<div id="main" class="m-all t-3of3 d-7of7 cf" role="main">
 
-			<?php 
-				// GET MARQUEE ( SAME AS HOME PAGE )
-				include('php/marquee/marquee.php');
-			?>
-
 			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
+			<div class="marquee cf shadow">
+			    <div class="marquee-holder cf">
+			        
+			        <div class="logo-holder">
+			            <div class="logo marquee-logo"></div>
+			        </div>
 
+			        <h2 class="marq-2"><span style="font-family: minion-pro,serif;">the</span></h2>
+
+			        <h1>SELECTED</h1>
+			        <hr>
+
+			        <h2 class="white-headline">STAND OUT &amp; GET SELECTED</h2>
+
+			        <div class="contact-content cf">
+			        	<?php the_content(); ?>
+			        </div>
+
+			    </div> <!-- END MARQUEE-HOLDER -->
+			</div><!-- END MARQUEE -->
 
 			<?php endwhile;?>
 
